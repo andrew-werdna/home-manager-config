54467cfbdf14ab5d7d5ddab91ed37ef2bbeb0aeb
andrew-werdna <8261769+andrew-werdna@users.noreply.github.com>
2023-11-30 20:51:47 -0600

    formatting home.nix

--- home.nix
+++ home.nix
@@ -220,7 +220,8 @@
         };
         pager = { difftool = true; };
       };
-      hooks = { pre-commit = ./pre-commit; }; # I need to investigate a per-repository git hooks setup
+      # I need to investigate a per-repository git hooks setup
+      hooks = { pre-commit = ./pre-commit; }; 
       userName = "andrew-werdna";
       userEmail = "8261769+andrew-werdna@users.noreply.github.com";
     };
