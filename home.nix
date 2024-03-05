fd4f784e353102846c094cc1a91c7fff64bedc7a
abrown7100 <129974093+abrown7100@users.noreply.github.com>
2024-03-04 19:20:08 -0600

    add global alias; update existing alias;

--- home.nix
+++ home.nix
@@ -370,10 +370,11 @@
 
         # Misc
         _update = "sudo apt update -y && sudo apt upgrade -y --allow-downgrades && sudo apt autoclean -y && sudo apt autoremove -y";
-        gitupdatehome = ''
+        updatehomerepos = ''
           find "$HOME" -maxdepth 3 -type d -name .git -exec dirname {} \; | xargs -I {} git -P -C {} pull -tp
         '';
         gitp = "git -P";
+        getLatestGitTag = "gitp tag | tail -n 1";
         hmedit = "hx ~/.config/home-manager/";
         reshell = "exec $SHELL";
 
